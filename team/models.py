6a4f86c3bf436d441d0cdb7f387b7c8475404385
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-17 22:46:45 +0300

    :sparkles: Added team models

--- team/models.py
+++ team/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Type(models.Model):
+    name = models.CharField(
+        max_length=63, unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Team(models.Model):
+    name = models.CharField(
+        max_length=63
+    )
+    type = models.ForeignKey(
+        to=Type,
+        related_name="teams",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    class Meta:
+        unique_together = ("name", "type")
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    TYPES = (
+        ("Pending", "Pending"),
+        ("In Progress", "In Progress"),
+        ("Done", "Done"),
+    )
+
+    name = models.CharField(
+        max_length=63
+    )
+    description = models.TextField()
+    status = models.CharField(
+        max_length=63, choices=TYPES
+    )
+    team = models.ForeignKey(
+        to=Team,
+        related_name="tasks",
+        on_delete=models.SET_NULL,
+        null=True
+    )
+
+    class Meta:
+        unique_together = ("name", "team")
+
+    def __str__(self):
+        return self.name
