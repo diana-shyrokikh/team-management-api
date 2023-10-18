3652bb0c8d2f5b78e60892266bd2bfb86010dbe9
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-18 19:52:46 +0300

    :sparkles: Added user view

--- user/views.py
+++ user/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import viewsets
 
-# Create your views here.
+from user.serializers import (
+    UserSerializer,
+    UserMiniSerializer,
+    UserDetailUpdateSerializer,
+)
+
+
+class UserView(viewsets.ModelViewSet):
+    queryset = get_user_model().objects.select_related(
+        "team"
+    )
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return UserMiniSerializer
+
+        if self.action in ("update", "retrieve"):
+            return UserDetailUpdateSerializer
+
+        return UserSerializer
+
