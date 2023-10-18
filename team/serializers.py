192c59697a60019e3cdc9c64af5ee6db59f86198
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-18 12:07:15 +0300

    :safety_vest: Added validation for TypeSerializer

--- team/serializers.py
+++ team/serializers.py
@@ -1,9 +1,22 @@
 from rest_framework import serializers
 
 from team.models import Type
+from team_management.validators import validate_name
 
 
 class TypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Type
         fields = "__all__"
+
+    def validate(self, attrs):
+        data = super(
+            TypeSerializer, self
+        ).validate(attrs)
+
+        validate_name(
+            name=attrs["name"],
+            field_name="name"
+        )
+
+        return data
