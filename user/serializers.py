899b2502c13afc2aeef7ea20e1e5ccef7e54b7f2
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-18 19:48:57 +0300

    :sparkles: Added user serializers

---
+++ user/serializers.py
@@ -0,0 +1,80 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from team.models import Team
+from team_management.validators import validate_user_name
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "email",
+            "first_name",
+            "last_name",
+            "password"
+        )
+
+    def validate(self, attrs):
+        data = super(
+            UserSerializer, self
+        ).validate(attrs)
+
+        if attrs["first_name"]:
+            validate_user_name(
+                name=attrs["first_name"],
+                field_name="first_name"
+            )
+
+        if attrs["last_name"]:
+            validate_user_name(
+                name=attrs["last_name"],
+                field_name="last_name"
+            )
+
+        return data
+
+    def create(self, validated_data):
+        return get_user_model().objects.create_user(**validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop("password", None)
+        user = super().update(instance, validated_data)
+
+        if password:
+            user.set_password(password)
+            user.save()
+
+        return user
+
+
+class UserDetailUpdateSerializer(serializers.ModelSerializer):
+    team = serializers.SlugRelatedField(
+        many=False,
+        read_only=False,
+        slug_field="name",
+        allow_null=True,
+        queryset=Team.objects.all()
+    )
+
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "first_name",
+            "last_name",
+            "email",
+            "team",
+            "is_leader"
+        )
+
+
+class UserMiniSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = get_user_model()
+        fields = (
+            "id",
+            "full_name",
+        )
