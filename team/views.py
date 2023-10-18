608c5b1a1a01bfd1c39dbef4f2a3c4d9481c070c
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-18 14:07:05 +0300

    :sparkles: Added team view

--- team/views.py
+++ team/views.py
@@ -1,9 +1,22 @@
 from rest_framework import viewsets
 
-from team.models import Type
-from team.serializers import TypeSerializer
+from team.models import (
+    Type,
+    Team,
+)
+from team.serializers import (
+    TypeSerializer,
+    TeamSerializer,
+)
 
 
 class TypeView(viewsets.ModelViewSet):
     queryset = Type.objects.all()
     serializer_class = TypeSerializer
+
+
+class TeamView(viewsets.ModelViewSet):
+    queryset = Team.objects.select_related(
+        "leader", "type"
+    )
+    serializer_class = TeamSerializer
