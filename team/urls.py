21aa37bd3dcb4d1e31fe0c82a5de83ebb503a8a0
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-18 14:07:23 +0300

    :sparkles: Added team url in router

--- team/urls.py
+++ team/urls.py
@@ -1,13 +1,17 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from team.views import TypeView
+from team.views import (
+    TypeView,
+    TeamView,
+)
 
 app_name = "team"
 
 router = DefaultRouter()
 
 router.register("types", TypeView)
+router.register("team", TeamView)
 
 urlpatterns = [
     path("", include(router.urls)),
