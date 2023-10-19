46078cb66b4156f40d43310e9441ad159ad52c60
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-19 14:18:18 +0300

    :sparkles: Added authorization urls

--- user/urls.py
+++ user/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from user.views import (
     UserView,
@@ -18,5 +22,15 @@ urlpatterns = [
         "me/",
         UserMeView.as_view(),
         name="user-me"
+    ),
+    path(
+        "token/",
+        TokenObtainPairView.as_view(),
+        name="token_obtain_pair"
+    ),
+    path(
+        "token/refresh/",
+        TokenRefreshView.as_view(),
+        name="token_refresh"
     ),
 ]
