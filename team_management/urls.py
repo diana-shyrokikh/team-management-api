2963b2da9fbd6fda19a95302834923fbde013234
Diana Shyrokikh <diashiroks@gmail.com>
2023-10-20 15:01:37 +0300

    :sparkles: Added static path

--- team_management/urls.py
+++ team_management/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import (
@@ -53,4 +55,7 @@ urlpatterns = [
             "user.urls", namespace="user"
         ),
     ),
-]
+] + static(
+    settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT
+)
